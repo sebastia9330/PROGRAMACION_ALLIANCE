f6d0c2be4a3355801b8f3bb15138fb084baa8a0d
sebastian carrero <mugssebastian@gmail.com>
2024-10-09 15:21:10 -0500

    9/10/24/1

--- codigosSQL/glosarioComandosSQL.sql
+++ codigosSQL/glosarioComandosSQL.sql
@@ -3,3 +3,7 @@ ALTER TABLE nombre_de_tabla DROP COLUMN nombre_de_columna;
 
 --CAMBIAR EL NOMBRE DE UNA COLUMNA
 ALTER TABLE nombre_de_tabla RENAME COLUMN nombre_viejo TO nombre_nuevo;
+
+--modificar el tamaño de una columna
+ALTER TABLE nombre_tabla
+MODIFY nombre_columna VARCHAR2(nuevo_tamano);
