893179c9617dd3dcbc09bcb637dfd7ad66f8e07e
sebastian carrero <mugssebastian@gmail.com>
2024-10-09 07:44:48 -0500

    9/10/24

---
+++ codigosSQL/glosarioComandosSQL.sql
@@ -0,0 +1,5 @@
+--Borrar una columna
+ALTER TABLE nombre_de_tabla DROP COLUMN nombre_de_columna;
+
+--CAMBIAR EL NOMBRE DE UNA COLUMNA
+ALTER TABLE nombre_de_tabla RENAME COLUMN nombre_viejo TO nombre_nuevo;
